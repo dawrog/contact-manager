c08db758d140009042ed0e0f8fb5a3c5a89cda2e
dawrog <dawid.rogal@protonmail.com>
2018-01-21 23:59:45 +0100

    added new modules

--- src/App.js
+++ src/App.js
@@ -1,94 +1,94 @@
-import * as React from 'react';
-import { Component } from 'react';
-import './App.css';
+import * as React from "react";
+import { Component } from "react";
+import "./App.css";
 
 
 function AppHeader() {
-  return (
-    <div>
-      <nav className="navbar navbar-default">
-        <div className="col-md-offset-4 container-fluid">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            </button>
-        <a className="navbar-brand" href="#">Contact List</a>
-        </div>
+	return (
+		<div>
+			<nav className="navbar navbar-default">
+				<div className="col-md-offset-4 container-fluid">
+					<div className="navbar-header">
+						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span className="sr-only">Toggle navigation</span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+						</button>
+						<a className="navbar-brand" href="#">Contact List</a>
+					</div>
 
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav">
-            <li className="btn btn-default"><a href="#">Add <span class="sr-only">(current)</span></a></li>
-            <li className="btn btn-default"><a href="#">Link</a></li>
-              </ul>
-          </div>
-        </div>
-        </nav>
-    </div>
-  );
+					<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul className="nav navbar-nav">
+							<li className="btn btn-default"><a href="#">Add<span class="sr-only">(current)</span></a></li>
+							<li className="btn btn-default"><a href="#">Link</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
 }
 
 function ContactsList() {
-  return (
-    <div>
-      <div className="list-group text-center">
-      <ContactItem
-        avatarLogin="sunbites@gmail.com"
-        name="Matt"
-        department="Java Developer"
-      />
-      <ContactItem
-        avatarLogin="admin666"
-        name="Stephan"
-        department="UX Design"
-      />
-      <ContactItem
-        avatarLogin="qwerty123"
-        name="Maria"
-        department="Wordpress Developer"
-      />     
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="list-group text-center">
+				<ContactItem
+					avatarLogin="sunbites@gmail.com"
+					name="Matt"
+					department="Java Developer"
+				/>
+				<ContactItem
+					avatarLogin="admin666"
+					name="Stephan"
+					department="UX Design"
+				/>
+				<ContactItem
+					avatarLogin="qwerty123"
+					name="Maria"
+					department="Wordpress Developer"
+				/>     
+			</div>
+		</div>
+	);
 }
 
 function ContactItem( { avatarLogin, name, department } ) {
-  return (
-    <div className="item">
-      <a href="#" className="list-group-item">
-          <UserAvatar login={avatarLogin} />
-          <h4>{name}</h4> 
-          <p>{department}</p>
-      </a>
-    </div>
-  );
+	return (
+		<div className="item">
+			<a href="#" className="list-group-item">
+				<UserAvatar login={avatarLogin} />
+				<h4>{name}</h4> 
+				<p>{department}</p>
+			</a>
+		</div>
+	);
 }
  
 function UserAvatar( { login } ) {
-  let imgUrl;
+	let imgUrl;
  
-  if (login.includes("@")) {
-      imgUrl = "https://78.media.tumblr.com/avatar_4ed6e2f869e8_128.pnj";
+	if (login.includes("@")) {
+		imgUrl = "https://78.media.tumblr.com/avatar_4ed6e2f869e8_128.pnj";
 
-  } else {
-      imgUrl = "https://78.media.tumblr.com/avatar_37cec57dd00d_128.png";
+	} else {
+		imgUrl = "https://78.media.tumblr.com/avatar_37cec57dd00d_128.png";
 
- }
-  return (
-      <img src={imgUrl}/>
-  );
+	}
+	return (
+		<img src={imgUrl}/>
+	);
 }
 
 function App() {
-  return (
-      <div className="App">
-      <AppHeader />
-      <ContactsList />
-      </div>
+	return (
+		<div className="App">
+			<AppHeader />
+			<ContactsList />
+		</div>
       
-    );
-  }
+	);
+}
 
 export default App;
