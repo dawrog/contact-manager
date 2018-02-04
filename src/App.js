db3fbbcb4c6ab2b0bd1e04f024593b32278516c0
dawrog <dawid.rogal@protonmail.com>
2018-02-04 23:27:34 +0100

    added user list filer components

--- src/App.js
+++ src/App.js
@@ -29,6 +29,54 @@ function AppHeader() {
 	);
 }
 
+const allUsers = ["Michal", "Kasia", "Jacek", "Marta", "Tomek", "Ania"];
+
+class App extends React.Component {
+	constructor() {
+		super();    
+    
+		this.state = {
+			filteredUsers: allUsers
+		};
+	}
+
+	filterUsers(e) {
+		const text = e.currentTarget.value;
+		const filteredUsers = this.getFilteredUsersForText(text);
+		this.setState({
+			filteredUsers
+		});
+	}
+  
+	getFilteredUsersForText(text) {
+		return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()));
+	}
+  
+	render () {
+		return (
+			<div>
+				<input onInput={this.filterUsers.bind(this)} />
+				<UsersList users={this.state.filteredUsers} />
+			</div>
+		);
+	}
+}
+
+const UsersList = ({ users }) => {
+	if (users.length > 0) {
+		return (
+			<ul>
+				{users.map(user => <li key={user}>{user}</li>)}
+			</ul>
+		);
+	}
+
+	return (
+		<p>No results!</p>
+	);
+};
+
+
 class Parent extends React.Component {
 	constructor(props) {
 		super(props);
